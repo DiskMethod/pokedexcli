b10fd763a13d6194e53ddc1420ef3f4b0d02ea7e
Lucas Chagas <lucas.w.chagas@gmail.com>
2023-12-23 23:56:04 -0500

    Added simple cache

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,61 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+type Cache struct {
+	Cache map[string]cacheEntry
+	sync.RWMutex
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val []byte
+}
+
+func (c *Cache) Set(key string, value []byte) {
+	c.Lock()
+	defer c.Unlock()
+
+	c.Cache[key] = cacheEntry{
+		createdAt: time.Now(),
+		val: value,
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.RLock()
+	defer c.RUnlock()
+
+	entry, ok := c.Cache[key]
+	return entry.val, ok
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for {
+		<-ticker.C
+		c.cleanup(interval)
+	}
+}
+
+func (c *Cache) cleanup(interval time.Duration) {
+	c.Lock()
+	defer c.Unlock()
+
+	now := time.Now()
+	for key, val := range c.Cache {
+		if now.Sub(val.createdAt) > interval {
+			delete(c.Cache, key)
+		}
+	}
+}
+
+func NewCache(interval time.Duration) *Cache {
+	cache := &Cache{
+		Cache: map[string]cacheEntry{},
+	}
+	go cache.reapLoop(interval)
+	return cache
+}
